e785701ade35670393826613bc4d1268f8af4fb6
TheScientist33 <100351431+TheScientist33@users.noreply.github.com>
2026-01-17 14:42:27 +0100

    deploy: update web build after swipe fixes

--- swipewatch/docs/flutter_bootstrap.js
+++ swipewatch/docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"5517cc9b3b3bcf12431b47f495e342a30b7388
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3288149431"
+    serviceWorkerVersion: "1896099170"
   }
 });
--- swipewatch/docs/flutter_service_worker.js
+++ swipewatch/docs/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3e49a48a7120e0702ed9e089a24dc1db
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ac087b752cc0a9e6539d521d79e274e1",
+"flutter_bootstrap.js": "9870fdce40fec3163eaedc8a4d66c875",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "48963c2246d70aa52c21997e1f8d5058",
 "/": "48963c2246d70aa52c21997e1f8d5058",
-"main.dart.js": "5871d0cd32108fa2672a3683addf596b",
+"main.dart.js": "45ab2fd9d10cbf71b78671cc0cf0f907",
 "manifest.json": "a0e8ca5e0b1de723b0786e893aa65a96",
 "version.json": "9851ba2575a5de4528557f64bdea2ac4"};
 // The application shell files that are downloaded before a service worker can
